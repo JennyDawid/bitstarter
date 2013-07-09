44af7dc127d588c29c29ca7799aa49210fd8cada
Jenny Dawid <joannadawid@gmail.com>
2013-07-09 19:48:14 +0000

    Upgraded web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var temp = fs.readFileSync('index.html');
+temp = buf.toString(temp);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(temp);
 });
 
 var port = process.env.PORT || 5000;
