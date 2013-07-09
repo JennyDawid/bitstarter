4c854b7610b53da5c694f26129acb270d0c2d1d2
Jenny Dawid <joannadawid@gmail.com>
2013-07-09 20:02:34 +0000

    a

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
-var fs = require('fs');
+
 
 var app = express.createServer(express.logger());
 
-var temp = fs.readFileSync('index.html');
-temp = buf.toString(temp);
+fs = fs.require('fs');
+var temp = fs.readFileSync('./index.html', 'utf-8');
+
 
 app.get('/', function(request, response) {
   response.send(temp);
